c914c73d8a8ec4564e6d529e1fcf9a397a140484
syeon111 <psooy19@gmail.com>
2025-09-19 18:31:53 +0900

    커뮤니티 게시글 이미지 dto 수정

--- rentalService/src/main/java/com/itemrental/rentalService/community/dto/response/CommunityPostCreateResponseDto.java
+++ rentalService/src/main/java/com/itemrental/rentalService/community/dto/response/CommunityPostCreateResponseDto.java
@@ -3,12 +3,15 @@ package com.itemrental.rentalService.community.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class CommunityPostCreateResponseDto {
   private Long id;
-  private String message;
+  private String username;
   private String title;
   private String content;
+  private List<String> imageUrls;
 }
 
